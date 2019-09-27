d1acffaa4029120a521e0e9b45749ea57b439889
Gabriel Antonia <Gabriel.Antonia@Outlook.com>
2019-09-27 16:49:26 -0600

    built jpa and rest project structure

---
+++ JPAEventTracker/src/test/java/com/skilldistillery/events/entities/EventTester.java
@@ -0,0 +1,52 @@
+package com.skilldistillery.events.entities;
+
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class EventTester {
+
+	private static EntityManagerFactory emf;
+	static EntityManager em;
+	static Event event;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("eventtracker");
+			
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		event = em.find(Event.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+	}
+
+	@Test
+	@DisplayName("test id")
+	void test() {
+		assertNotNull(event);
+	}
+
+}
